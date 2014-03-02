04d27129df413398b63ff8e46751cd59b3800e48
kakikubo teruo <kakikubo@gmail.com>
2014-03-02 17:31:02 +0900

    initial commit

---
+++ stopwatch.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+Shoes.app height: 200, width: 300 do
+  background lightblue
+  stack margin: 10 do
+    caption strong "Shoes ストップウォッチ", :font => "osaka"
+
+    stack do
+      button "start" do
+        @time = Time.now
+        @label.replace "#{@time.strftime '%l:%M:%S %p'}にスタート", :font => "osaka"
+      end
+
+      button "stop" do
+        @label.replace "ストップ:", strong("#{Time.now - @time}"),
+        "秒経過", :font => "osaka"
+      end
+    end
+    @label = para strong("スタート", :font => "osaka"), "ボタンをおすと開始します", :font => "osaka"
+  end
+end
